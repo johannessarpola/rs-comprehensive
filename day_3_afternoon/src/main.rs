4b141268616fa3df5eb80343efd4c5b4d77ad7e7
Johannes Sarpola <johannes.sarpola@qalmari.fi>
2024-04-05 17:13:17 +0300

    d2a pt2

--- day_3_afternoon/src/main.rs
+++ day_3_afternoon/src/main.rs
@@ -20,6 +20,46 @@ impl Node {
     }
 }
 
+#[derive(Debug)]
+// If a data type stores borrowed data, it must be annotated with a lifetime:
+struct Highlight<'doc>(&'doc str);
+
+fn erase(text: String) {
+    println!("Bye {text}!");
+}
+
+
+#[derive(Debug)]
+struct Point(i32, i32);
+
+// Read &'a Point as “a borrowed Point which is valid for at least the lifetime a”.
+fn left_most<'a>(p1: &'a Point, p2: &'a Point) -> &'a Point {
+    if p1.0 < p2.0 {
+        p1
+    } else {
+        p2
+    }
+}
+
+fn cab_distance(p1: &Point, p2: &Point) -> i32 {
+    (p1.0 - p2.0).abs() + (p1.1 - p2.1).abs()
+}
+
+fn nearest<'a>(points: &'a [Point], query: &Point) -> Option<&'a Point> {
+    let mut nearest = None;
+    for p in points {
+        if let Some((_, nearest_dist)) = nearest {
+            let dist = cab_distance(p, query);
+            if dist < nearest_dist {
+                nearest = Some((p, dist));
+            }
+        } else {
+            nearest = Some((p, cab_distance(p, query)));
+        };
+    }
+    nearest.map(|(p, _)| p)
+}
+
 fn main() {
     let root = Node::new(1);
     root.borrow_mut().children.push(Node::new(5));
@@ -42,4 +82,25 @@ fn main() {
 
     println!("graph: {root:#?}");
     println!("graph sum: {}", root.borrow().sum());
+
+    let p1: Point = Point(10, 10);
+    let p2: Point = Point(20, 20);
+    let p3 = left_most(&p1, &p2); // What is the lifetime of p3?
+    println!("p3: {p3:?}");
+
+
+    println!(
+        "{:?}",
+        nearest(
+            &[Point(1, 0), Point(1, 0), Point(-1, 0), Point(0, -1),],
+            &Point(0, 2)
+        )
+    );
+
+    let text = String::from("The quick brown fox jumps over the lazy dog.");
+    let fox = Highlight(&text[4..19]);
+    let dog = Highlight(&text[35..43]);
+    // erase(text); // will fail at compile since it is borrowed in fox&dog
+    println!("{fox:?}");
+    println!("{dog:?}");
 }
